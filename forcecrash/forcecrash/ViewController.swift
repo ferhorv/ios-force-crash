4e5ad95ab87c722f9e2aec42d373b0537979704a
ferhorv <ferenchorvath@outlook.com>
2020-06-25 14:12:51 +0200

    initial commit

---
+++ forcecrash/forcecrash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  forcecrash
+//
+//  Created by Ferenc Horvath on 25.06.20.
+//  Copyright © 2020 Ferenc Horvath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
